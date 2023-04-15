5f0d5950addd5645d075fe1cedf5b9b5efba7b54
Cupid Valentine <westerngod@126.com>
2023-04-15 17:01:33 +0800

    feat: add support for upstash

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,6 @@
 
 // const inter = Inter({ subsets: ['latin'] })
 
-export default function Home() {
+export default async function Home() {
   return <div className="text-red-500">hello world</div>
 }
