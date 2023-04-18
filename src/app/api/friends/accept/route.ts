8512fcd5425427a25d9958aba48108db0bcdf1a0
Cupid Valentine <westerngod@126.com>
2023-04-18 23:38:03 +0800

    feat: compeleted accept friend

--- src/app/api/friends/accept/route.ts
+++ src/app/api/friends/accept/route.ts
@@ -3,6 +3,7 @@ import { z } from 'zod'
 import { HttpStatusCode } from 'axios'
 import { authOptions } from '@/lib/auth'
 import { fetchRedis } from '@/helpers/redis'
+import { db } from '@/lib/db'
 
 export async function POST(req: Request) {
   try {
@@ -13,15 +14,28 @@ export async function POST(req: Request) {
     if (!session)
       return new Response('Unauthorized', { status: HttpStatusCode.Unauthorized })
     // verify both users are not already friends
-    const isAlreadyFriends = await fetchRedis('sismember', `user:${session.user.id}:friends`)
+    const isAlreadyFriends = await fetchRedis('sismember', `user:${session.user.id}:friends`, idToAdd)
     if (isAlreadyFriends)
       return new Response('Already friends', { status: HttpStatusCode.BadRequest })
-    const hasFriendRequest = await fetchRedis('sismember', `user:${session.user.id}:incoming_friend_requests`, idToAdd)
 
-    console.log(hasFriendRequest)
+    const hasFriendRequest = await fetchRedis('sismember', `user:${session.user.id}:incoming_friend_requests`, idToAdd)
+    if (!hasFriendRequest)
+      return new Response('No friend request', { status: HttpStatusCode.BadRequest })
+
+    await db.sadd(`user:${session.user.id}:friends`, idToAdd)
+
+    await db.sadd(`user:${idToAdd}:friend`, session.user.id)
+
+    // await db.srem(`user:${idToAdd}:outbound_friend_requests`, session.user.id)
+    await db.srem(`user:${session.user.id}:incoming_friend_requests`, idToAdd)
+
+    return new Response('ok')
   }
 
   catch (error) {
+    if (error instanceof z.ZodError)
+      return new Response('Invalid request payload', { status: HttpStatusCode.UnprocessableEntity })
 
+    return new Response('Invalid request', { status: HttpStatusCode.BadRequest })
   }
 }
