583ca59dba05084916cb46100a00d62cb990aaeb
Cupid Valentine <westerngod@126.com>
2023-05-04 23:51:16 +0800

    chore: update deps

--- src/components/SidebarChatList.tsx
+++ src/components/SidebarChatList.tsx
@@ -25,10 +25,24 @@ export default function SidebarChatList({ friends, sessionId }: SidebarChatListP
   return (
     <ul role="list" className="max-h-[25rem] overflow-y-auto -mx-2 space-y-1">
       {friends.sort().map((friend) => {
-        const unseenMessagesCount = unseenMessages?.filter(unseenMsg => unseenMsg.senderId === friend.id).length
+        const unseenMessagesCount = unseenMessages?.filter(
+          unseenMsg => unseenMsg.senderId === friend.id,
+        ).length
         return (
           <li key={friend.id}>
-            <a href={`/dashboard/chat/${chatHrefConstructor(sessionId, friend.id)}`}>{friend.name} {unseenMessagesCount > 0 ? <div className='flex items-center justify-center w-4 h-4 text-xs font-medium text-white bg-indigo-600 rounded-full'>{ unseenMessagesCount}</div> : null }</a>
+            <a
+              href={`/dashboard/chat/${chatHrefConstructor(sessionId, friend.id)}`}
+              className="flex items-center p-2 text-sm font-semibold leading-6 text-gray-700 rounded-md hover:text-indigo-600 hover:bg-gray-50 group gap-x-3"
+            >
+              {friend.name}{' '}
+              {unseenMessagesCount > 0
+                ? (
+                <div className="flex items-center justify-center w-4 h-4 text-xs font-medium text-white bg-indigo-600 rounded-full">
+                  {unseenMessagesCount}
+                </div>
+                  )
+                : null}
+            </a>
           </li>
         )
       })}
